1665f767950daf75d8a43a4b7c13a6118415a12e
Eronheit <aquinoeronaldo@gmail.com>
2021-05-28 09:20:32 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy',
+  },
 };
