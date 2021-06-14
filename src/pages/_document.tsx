c98d59e48e13499e99c0c05508c9f34cd1e465d3
Eronheit <aquinoeronaldo@gmail.com>
2021-06-14 14:31:54 -0300

    chore: add next-pwa

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <meta name="theme-color" content="#fff" />
+          <link rel="apple-touch-icon" href="/logo.png" />
+          <link rel="manifest" href="manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
